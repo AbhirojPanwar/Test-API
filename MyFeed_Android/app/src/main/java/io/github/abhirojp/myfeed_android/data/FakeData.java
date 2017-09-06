0ea2acf6aa07b67e69064fcff611d5bdc4d9975f
abhiroj <abhiroj95@gmail.com>
2017-09-06 14:30:24 +0530

    Create fake data

---
+++ MyFeed_Android/app/src/main/java/io/github/abhirojp/myfeed_android/data/FakeData.java
@@ -0,0 +1,72 @@
+package io.github.abhirojp.myfeed_android.data;
+
+/**
+ * Created by abhiroj on 6/9/17.
+ * Purpose of the class is to provide a fake data until backend is setup
+ */
+
+public class FakeData {
+
+    private static final String apiData="[  \n" +
+            "   {  },\n" +
+            "   {  },\n" +
+            "   {  },\n" +
+            "   {  },\n" +
+            "   {  },\n" +
+            "   {  },\n" +
+            "   {  \n" +
+            "      name:\"Narendra Modi\",\n" +
+            "      imageUrl:\"https://pbs.twimg.com/profile_images/718314968102367232/ypY1GPCQ_400x400.jpg\",\n" +
+            "      title:\"People\",\n" +
+            "      text:\"Lorem ipsum dolor sit amet, consectetur adipiscing elit.\",\n" +
+            "      time:1501231779000,\n" +
+            "      description:\"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum, lectus non rhoncus tincidunt, quam\n" +
+            "lectus lacinia nunc, vitae egestas felis ligula a eros. Nulla ac ex finibus, sodales nisl ac, convallis urna. Donec ornare\n" +
+            "aliquam dapibus. Ut aliquam massa ex, a maximus lectus condimentum vitae. Donec rutrum aliquet ullamcorper. \n" +
+            "Suspendisse sit amet pulvinar tellus.\"\n" +
+            "   },\n" +
+            "   {  \n" +
+            "      name:\"Manali\",\n" +
+            "      imageUrl:\"https://www.holidify.com/images/compressed/4709.jpg\",\n" +
+            "      title:\"Place\",\n" +
+            "      time:1501231779000,\n" +
+            "      description:\"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum, lectus non rhoncus tincidunt, quam\n" +
+            "lectus lacinia nunc, vitae egestas felis ligula a eros. Nulla ac ex finibus, sodales nisl ac, convallis urna. Donec ornare\n" +
+            "aliquam dapibus. Ut aliquam massa ex, a maximus lectus condimentum vitae. Donec rutrum aliquet ullamcorper. \n" +
+            "Suspendisse sit amet pulvinar tellus.\"\n" +
+            "   },\n" +
+            "   {  \n" +
+            "      name:\"Greenhouse\",\n" +
+            "      text:\"Who loves a garden loves a greenhouse too\",\n" +
+            "      title:\"Quote\",\n" +
+            "      time:1500972579000,\n" +
+            "      description:\"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum, lectus non rhoncus tincidunt, quam\n" +
+            "lectus lacinia nunc, vitae egestas felis ligula a eros. Nulla ac ex finibus, sodales nisl ac, convallis urna. Donec ornare\n" +
+            "aliquam dapibus. Ut aliquam massa ex, a maximus lectus condimentum vitae. Donec rutrum aliquet ullamcorper. \n" +
+            "Suspendisse sit amet pulvinar tellus.\"\n" +
+            "   },\n" +
+            "   {  \n" +
+            "      name:\"Spiti Valley\",\n" +
+            "      imageUrl:\"https://www.holidify.com/images/compressed/349.jpg\",\n" +
+            "      title:\"Place\",\n" +
+            "      text:\"Lorem ipsum dolor sit amet, consectetur adipiscing elit.\",\n" +
+            "      time:1500972579000,\n" +
+            "      description:\"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum, lectus non rhoncus tincidunt, quam lectus\n" +
+            "lacinia nunc, vitae egestas felis ligula a eros. Nulla ac ex finibus, sodales nisl ac, convallis urna. Donec \n" +
+            "ornare aliquam dapibus. Ut aliquam massa ex, a maximus lectus condimentum vitae. Donec rutrum aliquet \n" +
+            "ullamcorper. Suspendisse sit amet pulvinar tellus.\"\n" +
+            "   },\n" +
+            "   {  \n" +
+            "      name:\"Narendra Modi\",\n" +
+            "      imageUrl:\"https://pbs.twimg.com/profile_images/718314968102367232/ypY1GPCQ_400x400.jpg\",\n" +
+            "      title:\"People\",\n" +
+            "      text:\"Lorem ipsum dolor sit amet, consectetur adipiscing elit.\",\n" +
+            "      time:1500972579000,\n" +
+            "      description:\"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum, lectus non rhoncus tincidunt, quam\n" +
+            "lectus lacinia nunc, vitae egestas felis ligula a eros. Nulla ac ex finibus, sodales nisl ac, convallis urna. Donec ornare\n" +
+            "aliquam dapibus. Ut aliquam massa ex, a maximus lectus condimentum vitae. Donec rutrum aliquet ullamcorper. \n" +
+            "Suspendisse sit amet pulvinar tellus.\"\n" +
+            "   }\n" +
+            "]";
+
+}
